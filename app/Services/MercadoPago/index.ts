30fa3446e6642441ad298da0f6678fbfe836560d
Gustavo Souza <gustavo.araujo@aluno.uepa.br>
2023-11-06 09:04:44 -0300

    fixed some stuff

--- app/Services/MercadoPago/index.ts
+++ app/Services/MercadoPago/index.ts
@@ -13,45 +13,51 @@ class MercadoPagoService {
   public async createPreference(promotional_code?: string, external_reference?: string) {
     const code = await PromotionalCode.findBy('slug', promotional_code)
 
-    const response = code
-      ? await preferences.create({
-          auto_return: 'approved',
-          back_urls: {
-            success: 'https://api.uepautd.online/marketplace/payments',
-            pending: 'https://api.uepautd.online/marketplace/payments',
-            failure: 'https://api.uepautd.online/marketplace/payments',
+    if (code && code.canBeUsed > 0) {
+      code.canBeUsed -= 1
+      await code.save()
+      const response = preferences.create({
+        auto_return: 'approved',
+        back_urls: {
+          success: 'https://api.uepautd.online/marketplace/payments',
+          pending: 'https://api.uepautd.online/marketplace/payments',
+          failure: 'https://api.uepautd.online/marketplace/payments',
+        },
+        ...(external_reference && { external_reference }),
+        items: [
+          {
+            title: 'Assinatura mensal - UTD',
+            quantity: 1,
+            currency_id: 'BRL',
+            unit_price: 12 * (1 - code.discount),
+            description: 'Compra de uma assinatura mensal válida de UTD (válida por 30 dias)',
+            category_id: 'virtual_goods',
           },
-          ...(external_reference && { external_reference }),
-          items: [
-            {
-              title: 'Assinatura mensal - UTD',
-              quantity: 1,
-              currency_id: 'BRL',
-              unit_price: 12 * (1 - code.discount),
-              description: 'Compra de uma assinatura mensal válida de UTD (válida por 30 dias)',
-              category_id: 'virtual_goods',
-            },
-          ],
-        })
-      : await preferences.create({
-          auto_return: 'approved',
-          back_urls: {
-            success: 'https://api.uepautd.online/marketplace/payments',
-            pending: 'https://api.uepautd.online/marketplace/payments',
-            failure: 'https://api.uepautd.online/marketplace/payments',
-          },
-          ...(external_reference && { external_reference }),
-          items: [
-            {
-              title: 'Assinatura mensal - UTD',
-              quantity: 1,
-              currency_id: 'BRL',
-              unit_price: 12,
-              description: 'Compra de uma assinatura mensal válida de UTD (válida por 30 dias)',
-              category_id: 'virtual_goods',
-            },
-          ],
-        })
+        ],
+      })
+
+      return response.response as CreatePreferenceResponse
+    }
+
+    const response = await preferences.create({
+      auto_return: 'approved',
+      back_urls: {
+        success: 'https://api.uepautd.online/marketplace/payments',
+        pending: 'https://api.uepautd.online/marketplace/payments',
+        failure: 'https://api.uepautd.online/marketplace/payments',
+      },
+      ...(external_reference && { external_reference }),
+      items: [
+        {
+          title: 'Assinatura mensal - UTD',
+          quantity: 1,
+          currency_id: 'BRL',
+          unit_price: 12,
+          description: 'Compra de uma assinatura mensal válida de UTD (válida por 30 dias)',
+          category_id: 'virtual_goods',
+        },
+      ],
+    })
     return response.response as CreatePreferenceResponse
   }
 
